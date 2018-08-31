7c2c6bbdafd855d7b7e41b809641aecdd33b7cf6
DawnImpulse <dawnimpulse@gmail.com>
2018-08-31 13:36:39 +0530

    New : Artist XML

---
+++ app/src/main/java/com/dawnimpulse/wallup/ArtistProfile.kt
@@ -0,0 +1,12 @@
+package com.dawnimpulse.wallup
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ArtistProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artist_profile)
+    }
+}
