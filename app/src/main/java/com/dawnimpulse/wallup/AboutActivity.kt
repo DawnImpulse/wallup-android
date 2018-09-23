d82cd67510b4a106153eea79b1ac72b3943019f7
DawnImpulse <dawnimpulse@gmail.com>
2018-09-23 21:23:28 +0530

    New: About screen init

---
+++ app/src/main/java/com/dawnimpulse/wallup/AboutActivity.kt
@@ -0,0 +1,13 @@
+package com.dawnimpulse.wallup
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AboutActivity : AppCompatActivity() {
+
+    // on create
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
